b30b8fc142b097f9d0678951420d300257d9303a
Kuiliang Zhang <kzhang@student.ubc.ca>
2023-10-13 00:42:18 -0700

    feat: add favicon

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>M.R. Resume</span>,
   project: {
     link: "https://github.com/Crayon-ShinChan/mr-resume",
   },
@@ -13,6 +13,20 @@ const config: DocsThemeConfig = {
   footer: {
     text: "M.R. Resume Documentation",
   },
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s – M.R. Resume",
+    };
+  },
+  head: (
+    <>
+      <meta
+        property="og:description"
+        content="A framework to create new generation resume with power of React and Markdown."
+      />
+      <link rel="icon" href="./favicon.ico" />
+    </>
+  ),
 };
 
 export default config;
